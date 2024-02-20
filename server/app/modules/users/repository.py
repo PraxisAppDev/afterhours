78a8acfeffe9dd6369f1b294247ac733563c8355
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-02-19 21:08:29 -0500

    crud operations for users collection

--- server/app/modules/users/repository.py
+++ server/app/modules/users/repository.py
@@ -1,6 +1,7 @@
 from app.database import database
-from app.modules.users.models import UserModel
+from app.modules.users.models import UpdateUserModel, UserModel
 from app.util import handle_object_id
+from bson import ObjectId
 
 class UserRepository:
   def __init__(self):
@@ -10,8 +11,30 @@ class UserRepository:
     cursor = self.collection.find()
     return list(map(lambda document: handle_object_id(document), await cursor.to_list(1000)))
 
+  async def find_one(self, id: str):
+    document = await self.collection.find_one({"_id": ObjectId(id)})
+    if document:
+      return handle_object_id(document)
+
+  async def add_one(self, user: UserModel):
+    document = dict(user)
+    document.pop("id")
+    result = await self.collection.insert_one(document)
+    if result:
+      return str(result.inserted_id)
+    
   # TODO
-  def add(self, user: UserModel):
-    return None
+  async def update_one(self, id: str, update: UpdateUserModel):
+    result = await self.collection.update_one(
+      {"_id": ObjectId(id)},
+      {"$set": dict(update)}
+    )
+    return result.n > 0
+  
+  # TODO - Add HuntId
+
+  async def delete_one(self, id: str):
+    result = await self.collection.delete_one({"_id": ObjectId(id)})
+    return result.n > 0
 
 repository = UserRepository()
--- server/app/modules/users/router.py
+++ server/app/modules/users/router.py
@@ -1,20 +1,77 @@
 from fastapi import APIRouter, HTTPException
 from app.modules.users.service import service
-from app.modules.users.models import UserModel, UserCollection
+from app.modules.users.models import UserModel, UserResponseModel
 
 router = APIRouter()
 
 @router.get("/")
 async def read_users(
-  status_code = 200,
-  response_model=UserCollection,
+  status_code=200,
+  response_model=UserResponseModel
 ):
-  return await service.get_users()
+  result = await service.get_users()
+  return UserResponseModel(
+    message="fetched users",
+    content=result
+  )
+
+@router.get("/{id}")
+async def find_user_by_id(
+  id: str,
+  status_code=200,
+  response_model=UserResponseModel
+):
+  result = await service.find_user_by_id(id)
+  if result:
+    return UserResponseModel(
+      message="found user",
+      content=result
+    )
+  else:
+    raise HTTPException(status_code=400, detail="user not found")
 
 @router.post("/add")
-async def add_user(user: UserModel, status_code = 201):
-  id = await service.add_user(user)
-  if id:
-    return {"id": id}
+async def add_user(
+  user: UserModel,
+  status_code = 201,
+  response_model=UserResponseModel
+):
+  result = await service.add_user(user)
+  if result:
+    return UserResponseModel(
+      message="successfully created user",
+      content=result
+    )
   else:
-    raise HTTPException(status_code=409, detail="User already exists")
+    raise HTTPException(status_code=409, detail="user already exists")
+
+@router.post("/{id}")
+async def update_user_by_id(
+  id,
+  user: UserModel,
+  status_code = 200,
+  response_model=UserResponseModel
+):
+  result = await service.update_user_by_id(id, user)
+  if result:
+    return UserResponseModel(
+      message="successfully updated user",
+      content=result
+    )
+  else:
+    raise HTTPException(status_code=400, detail="user does not exist")
+
+@router.delete("/{id}")
+async def delete_user_by_id(
+  id,
+  status_code=200,
+  response_model=UserResponseModel
+):
+  result = await service.delete_user_by_id(id)
+  if result:
+    return UserResponseModel(
+      message="successfully deleted user",
+      content=result
+    )
+  else:
+    raise HTTPException(status_code=400, detail="user not found")
