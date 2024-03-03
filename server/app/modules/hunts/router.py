0ac9dec45c788d5e05ee8731197f16d54662ba00
Saloni <76454453+SalSaloni@users.noreply.github.com>
2024-03-03 13:58:01 -0500

    Revert "Add new users & create join_hunt"

--- server/app/modules/hunts/router.py
+++ server/app/modules/hunts/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from app.modules.hunts.service import service
 from app.modules.hunts.router_models import HuntModel, HuntResponseModel
-from app.modules.users.auth.router_models import AuthSuccessTextModel
 
 router = APIRouter()
 
@@ -26,16 +25,5 @@ async def load_past_hunts():
   result = await service.get_past()
   return HuntResponseModel(
     message="fetched hunts",
-    content=result
-  )
-
-@router.post(
-  "/join_hunt",
-  status_code=200,
-  response_model=HuntResponseModel
-)
-async def load_upcoming_hunts():
-  result = await service.get_upcoming()
-  return HuntResponseModel(
     content=result
   )
