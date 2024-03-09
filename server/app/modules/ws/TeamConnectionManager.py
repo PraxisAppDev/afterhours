4e41f2567d0c0622f877df2304bbc0092155a5f9
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-03-09 14:13:21 -0500

    teamconnectionmanager changes

--- server/app/modules/ws/TeamConnectionManager.py
+++ server/app/modules/ws/TeamConnectionManager.py
@@ -1,21 +1,49 @@
 from enum import StrEnum
-from typing import Dict, List
+import logging
+from typing import Dict, List, Tuple
 from fastapi import WebSocket
 from collections import defaultdict
 
-# Only care about HUNTTEAM & HUNTTEAMJOINREQUEST for now
+import pymongo
+from app import database
+
+# Only care about HUNTTEAMINFO & HUNTTEAMJOINREQUEST for now
+
 class TeamRequestType(StrEnum):
-  HUNTTEAMINFO = "HUNTTEAMINFO"
   HUNTTEAMJOINREQUEST = "HUNTTEAMJOINREQUEST"
-  HUNTQUESTIONSTATUS = "HUNTQUESTIONSTATUS"
+  HUNTTEAMACCEPTREQUEST = "HUNTTEAMACCEPTREQUEST"
+
+class TeamListenerType(StrEnum):
+  HUNTTEAMINFO = "HUNTTEAMINFO"
+  HUNTTEAMJOINREQUESTS = "HUNTTEAMJOINREQUESTS"
 
 type TEAM_ID = str
+
+# class TeamDBChangeStream:
+#   def __init__(self):
+#     self.collection = database.get_collection("teams")
+#     self.pipeline = [{ '$match': { 'operationType': ['insert', 'delete'] } }]
+#     self.resume_token = None
+  
+#   async def watch_collection(self):
+#     try:
+#       # Change collection name later
+#       async with self.collection.watch(self.pipeline, self.resume_token) as stream:
+#         async for insert_change in stream:
+#           print(insert_change)
+#           self.resume_token = stream.resume_token
+#     except pymongo.errors.PyMongoError:
+#       if not self.resume_token:
+#         logging.error('Failure in stream initialization')
+#       else:
+#         self.watch_collection()
+
 
 class TeamConnectionManager:
   def __init__(self):
     self.active_connections: Dict[
       TEAM_ID,
-      List[WebSocket]
+      List[Tuple[WebSocket, List[TeamListenerType]]]
     ] = defaultdict(list)
 
   async def connect(self, ws: WebSocket):
@@ -26,7 +54,8 @@ class TeamConnectionManager:
 
   def register_connection(self, init: object, ws: WebSocket):
     teamId = init["teamId"]
-    self.active_connections[teamId].append(ws)
+    listenerTypes = init["listenerTypes"]
+    self.active_connections[teamId].append((ws, listenerTypes))
 
   async def handle_request(self, request: object):
     type = request["type"]
@@ -35,7 +64,12 @@ class TeamConnectionManager:
     # TODO - Might instead add it to a queue of connections to remove
     self.clean(ws)
   
-  def clean(self, ws):
+  def clean(self, ws: WebSocket):
     for team_bucket in self.active_connections.values():
-      if ws in team_bucket:
-        team_bucket.remove(ws)
+      i = 0
+      while i < len(team_bucket):
+        if ws == team_bucket[i][0]:
+          break
+        i += 1
+      if i < len(team_bucket):
+        team_bucket.pop(i)
--- server/test/ws/test_ws.py
+++ server/test/ws/test_ws.py
@@ -1,11 +1,13 @@
 import random
 import string
+from app.modules.ws.TeamConnectionManager import TeamListenerType
 from test.client import client
 
 def test_team_websocket_connection():
   with client.websocket_connect("/ws/stream") as websocket:
     payload = {
-      "teamId": ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+      "teamId": ''.join(random.choices(string.ascii_uppercase + string.digits, k=20)),
+      "listenerTypes": [TeamListenerType.HUNTTEAMINFO]
     }
     websocket.send_json(payload)
     data = websocket.receive_json()
