52e0f0b10996599c18eba1b1942da32b19c51527
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-03-09 23:10:29 -0500

    team join and accept proof of concept

--- server/app/modules/ws/TeamConnectionManager.py
+++ server/app/modules/ws/TeamConnectionManager.py
@@ -1,23 +1,131 @@
 from enum import StrEnum
-import logging
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 from fastapi import WebSocket
 from collections import defaultdict
 
-import pymongo
-from app import database
+from pydantic import BaseModel
+
+# import pymongo
+# from app import database
 
 # Only care about HUNTTEAMINFO & HUNTTEAMJOINREQUEST for now
 
+type TEAM_ID = str
+type USER_ID = str
+
 class TeamRequestType(StrEnum):
-  HUNTTEAMJOINREQUEST = "HUNTTEAMJOINREQUEST"
-  HUNTTEAMACCEPTREQUEST = "HUNTTEAMACCEPTREQUEST"
+  TEAMJOINREQUEST = "HUNTTEAMJOINREQUEST"
+  TEAMACCEPTREQUEST = "HUNTTEAMACCEPTREQUEST"
 
 class TeamListenerType(StrEnum):
-  HUNTTEAMINFO = "HUNTTEAMINFO"
-  HUNTTEAMJOINREQUESTS = "HUNTTEAMJOINREQUESTS"
+  TEAMINFO = "TEAMINFO"
+  TEAMJOINREQUESTS = "TEAMJOINREQUESTS"
+  TEAMACCEPTREQUESTS = "TEAMACCEPTREQUESTS"
 
-type TEAM_ID = str
+class TeamErrorMessage(StrEnum):
+  INVALID_REQUEST = "INVALID_REQUEST"
+  INVALID_TOKEN = "INVALID_TOKEN"
+  UPGRADE_API_VERSION = "UPGRADE_API_VERSION"
+  INVALID_API_VERSION = "INVALID_API_VERSION"
+
+class InitRequestMessage(BaseModel):
+  teamId: str
+  userId: str
+  listenTo: List[TeamListenerType]
+  protocolExtensions: List[str]
+
+class InitResponseBaseMessage(BaseModel):
+  success: bool
+
+class InitResponsSuccessMessage(InitResponseBaseMessage):
+  protocolExtensions: List[str]
+
+class InitReponseFailureMessage(InitResponseBaseMessage):
+  errorMessage: TeamErrorMessage
+
+class TeamRequestMessage(BaseModel):
+  type: TeamRequestType
+  teamId: str
+
+class TeamRequestJoinMessage(TeamRequestMessage):
+  joinerId: str
+
+class TeamRequestAcceptMessage(TeamRequestMessage):
+  acceptedUserId: str
+
+class TeamJoinRequest(BaseModel):
+  userId: str
+
+class TeamJoinRequestResponse(BaseModel):
+  success: bool
+
+# Class that handles all the realtime logic for team operations
+class TeamConnectionManager:
+  def __init__(self):
+    self.active_connections: Dict[
+      TEAM_ID,
+      List[List[Union[USER_ID, WebSocket, List[TeamListenerType]]]]
+    ] = defaultdict(list)
+
+  async def connect(self, ws: WebSocket):
+    await ws.accept()
+
+    init = await ws.receive_json()
+    self.register_connection(init, ws)
+
+    response = InitResponsSuccessMessage(
+      success=True,
+      protocolExtensions=[]
+    ).model_dump()
+
+    await ws.send_json(response)
+
+  def register_connection(self, init: InitRequestMessage, ws: WebSocket):
+    teamId = init["teamId"]
+    listenTo = init["listenTo"]
+    userId = init["userId"]
+    self.active_connections[teamId].append([userId, ws, listenTo])
+
+  async def handle_request(self, request: TeamRequestMessage):
+    type = request["type"]
+    teamId = request["teamId"]
+
+    if type == TeamRequestType.TEAMJOINREQUEST:
+      joinerId = request["joinerId"]
+      for userId, ws, listenTo in self.active_connections[teamId]:
+        if TeamListenerType.TEAMJOINREQUESTS in listenTo:
+          join_request = TeamJoinRequest(
+            userId=joinerId
+          )
+          await ws.send_json(join_request.model_dump())
+    
+    if type == TeamRequestType.TEAMACCEPTREQUEST:
+      acceptedUserId = request["acceptedUserId"]
+      for userId, ws, listenTo in self.active_connections[teamId]:
+        if TeamListenerType.TEAMACCEPTREQUESTS in listenTo and userId == acceptedUserId:
+          # TODO - Add meaningful resposne
+          listenTo.pop()
+          listenTo.append(TeamListenerType.TEAMINFO)
+          response = TeamJoinRequestResponse(
+            success=True
+          )
+          await ws.send_json(response.model_dump())
+
+  def disconnect(self, ws: WebSocket):
+    # TODO - Might instead add it to a queue of connections to remove
+    self.clean(ws)
+
+  def clean(self, ws: WebSocket):
+    for team_bucket in self.active_connections.values():
+      i = 0
+
+      while i < len(team_bucket):
+        if ws == team_bucket[i][0]:
+          break
+        i += 1
+      
+      if i < len(team_bucket):
+        team_bucket.pop(i)
 
 # class TeamDBChangeStream:
 #   def __init__(self):
@@ -37,39 +145,3 @@ type TEAM_ID = str
 #         logging.error('Failure in stream initialization')
 #       else:
 #         self.watch_collection()
-
-
-class TeamConnectionManager:
-  def __init__(self):
-    self.active_connections: Dict[
-      TEAM_ID,
-      List[Tuple[WebSocket, List[TeamListenerType]]]
-    ] = defaultdict(list)
-
-  async def connect(self, ws: WebSocket):
-    await ws.accept()
-    init = await ws.receive_json()
-    self.register_connection(init, ws)
-    await ws.send_json({"message": "connection established"})
-
-  def register_connection(self, init: object, ws: WebSocket):
-    teamId = init["teamId"]
-    listenerTypes = init["listenerTypes"]
-    self.active_connections[teamId].append((ws, listenerTypes))
-
-  async def handle_request(self, request: object):
-    type = request["type"]
-
-  def disconnect(self, ws: WebSocket):
-    # TODO - Might instead add it to a queue of connections to remove
-    self.clean(ws)
-  
-  def clean(self, ws: WebSocket):
-    for team_bucket in self.active_connections.values():
-      i = 0
-      while i < len(team_bucket):
-        if ws == team_bucket[i][0]:
-          break
-        i += 1
-      if i < len(team_bucket):
-        team_bucket.pop(i)
--- server/test/ws/test_ws.py
+++ server/test/ws/test_ws.py
@@ -1,14 +1,76 @@
 import random
 import string
-from app.modules.ws.TeamConnectionManager import TeamListenerType
+from app.modules.ws.TeamConnectionManager import InitRequestMessage, TeamListenerType, TeamRequestAcceptMessage, TeamRequestJoinMessage, TeamRequestMessage, TeamRequestType
 from test.client import client
 
 def test_team_websocket_connection():
   with client.websocket_connect("/ws/stream") as websocket:
-    payload = {
-      "teamId": ''.join(random.choices(string.ascii_uppercase + string.digits, k=20)),
-      "listenerTypes": [TeamListenerType.HUNTTEAMINFO]
-    }
-    websocket.send_json(payload)
+    payload = InitRequestMessage(
+      teamId=''.join(random.choices(string.ascii_uppercase + string.digits, k=20)),
+      userId=''.join(random.choices(string.ascii_uppercase + string.digits, k=20)),
+      listenTo=[TeamListenerType.TEAMINFO],
+      protocolExtensions=[]
+    )
+    websocket.send_json(payload.model_dump())
     data = websocket.receive_json()
-    assert data == {"message": "connection established"}
+    assert data == {
+      "success": True,
+      "protocolExtensions": []
+    }
+
+def test_team_websocket_join_and_accept_request():
+  teamId = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+  leaderId = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+  joinerId = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+
+  with client.websocket_connect("/ws/stream") as leader:
+    with client.websocket_connect("/ws/stream") as joiner:
+      payload = InitRequestMessage(
+        teamId=teamId,
+        userId=leaderId,
+        listenTo=[TeamListenerType.TEAMINFO, TeamListenerType.TEAMJOINREQUESTS],
+        protocolExtensions=[]
+      )
+      leader.send_json(payload.model_dump())
+      data = leader.receive_json()
+      assert data == {
+        "success": True,
+        "protocolExtensions": []
+      }
+
+      payload = InitRequestMessage(
+        teamId=teamId,
+        userId=joinerId,
+        listenTo=[TeamListenerType.TEAMACCEPTREQUESTS],
+        protocolExtensions=[]
+      )
+      joiner.send_json(payload.model_dump())
+      data = joiner.receive_json()
+      assert data == {
+        "success": True,
+        "protocolExtensions": []
+      }
+
+      payload = TeamRequestJoinMessage(
+        type=TeamRequestType.TEAMJOINREQUEST,
+        teamId=teamId,
+        joinerId=joinerId
+      )
+      joiner.send_json(payload.model_dump())
+
+      data = leader.receive_json()
+      assert data == {
+        "userId": joinerId
+      }
+
+      payload = TeamRequestAcceptMessage(
+        type=TeamRequestType.TEAMACCEPTREQUEST,
+        teamId=teamId,
+        acceptedUserId=data["userId"]
+      )
+      leader.send_json(payload.model_dump())
+
+      data = joiner.receive_json()
+      assert data == {
+        "success": True
+      }
