115c3f9858610423afbc84975358934f8a540f6a
Saloni <salonigupta12405@gmail.com>
2024-03-11 15:04:58 -0400

    Team tokens

--- server/app/modules/teams/router.py
+++ server/app/modules/teams/router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from app.modules.teams.service import service
 from app.modules.teams.router_models import TeamsResponseModel
 from app.modules.teams.team_models import Team
+from app.modules.users.auth.router_models import Token
+from app.modules.users.auth.util import create_access_token
 
 router = APIRouter()
 
@@ -14,14 +16,18 @@ router = APIRouter()
       "description": "Error creating hunt",
       "model": TeamsResponseModel
     }
+    
   }
 )
 async def create_team(request: Team):
-  print(request)
   inserted_hunt_id = await service.create_team(request)
   return TeamsResponseModel(
     message="creating team",
-    content=inserted_hunt_id
+    content=inserted_hunt_id,
+    token=Token(
+          access_token=create_access_token({"_id": inserted_hunt_id}),
+          token_type="bearer"
+    )
   )
 
 
