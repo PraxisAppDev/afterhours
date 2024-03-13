b4eb2565e29f40f6ba40d1ae465d31d408dd250c
Aidan Melvin <aidmelvin.314@gmail.com>
2024-03-13 18:29:32 -0400

    changed name of hunt schema

--- server/app/modules/hunts/router.py
+++ server/app/modules/hunts/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from app.modules.hunts.service import service
 from app.modules.hunts.router_models import HuntModel, HuntResponseModel, HuntCreatedSuccessfullyModel, HuntCreationErrorModel
-from app.modules.hunts.hunt_models import ChallengeSchema
+# from app.modules.hunts.hunt_models import ChallengeSchema
+from app.modules.hunts.hunt_models import HuntSchema
 from app.modules.users.auth.router_models import AuthSuccessTextModel
 
 router = APIRouter()
@@ -43,8 +44,7 @@ async def load_past_hunts():
     }
   }
 )
-async def create_hunt(request: ChallengeSchema):
-  print(request)
+async def create_hunt(request: HuntSchema):
   inserted_hunt_id = await service.create_hunt(request)
   return HuntCreatedSuccessfullyModel(message="Hunt created successfully", inserted_hunt_id=inserted_hunt_id)
 
