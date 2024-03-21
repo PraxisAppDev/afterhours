3f8994e9e0854c99237e9c44fe948a2b9868196a
Om Duggineni <omduggineni@gmail.com>
2024-03-21 00:49:35 -0400

    server implementation of streaming request

--- server/app/util.py
+++ server/app/util.py
@@ -1,3 +1,14 @@
+from typing import AsyncIterator
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+
 def handle_object_id(document):
   document["_id"] = str(document["_id"])
   return document
+
+def stream_jsonl_response(stream: AsyncIterator[BaseModel]) -> StreamingResponse:
+  async def mapped_stream(stream_to_map):
+    async for obj in stream_to_map:
+      yield obj.model_dump_json() + '\n'
+  return StreamingResponse(mapped_stream(stream), media_type="application/x-ndjson")
