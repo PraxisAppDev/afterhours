e8e766be2b9eb24120f53608da1b2b69dbe7c889
Saloni <salonigupta12405@gmail.com>
2024-03-20 18:41:23 -0700

    main script modified

--- server/scripts/main_script.py
+++ server/scripts/main_script.py
@@ -1,6 +1,15 @@
-exec(open("py_add_users.py").read())
-exec(open("add_hunts.py").read())
-#for hunt_team remember to update the hunt_id in add_teams.py
-#according to the hunt_id in mongo comapss. 
-exec(open("add_teams.py").read())
+from py_add_users import Add_Users
+from add_hunts import Add_Hunts
+from add_teams import Add_Teams
+
+Add_Users()
+
+hunter = Add_Hunts()
+hunt_id = hunter.do()
+
+teams = Add_Teams()
+#Replace this string with team leader id from front-end
+teams.add(hunt_id, "65e8d7479bf978a5b7c2dfbb")
+
+
 
