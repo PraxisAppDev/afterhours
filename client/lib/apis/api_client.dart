72e0a4becbcaa26f2b3245b883a53847c4b82ad0
Om Duggineni <omduggineni@gmail.com>
2024-03-21 00:49:25 -0400

    client implementation of streamed request

--- client/lib/apis/api_client.dart
+++ client/lib/apis/api_client.dart
@@ -1,4 +1,8 @@
 import 'package:http/http.dart' as http;
+import 'api_utils/get_http_client/get_http_client_default.dart'
+    if (dart.library.io) 'api_utils/get_http_client/get_http_client_io.dart'
+    if (dart.library.html) 'api_utils/get_http_client/get_http_client_html.dart'
+    as get_http_client;
 
 var apiUrl = "http://localhost:8001";
-var client = http.Client();
+var client = get_http_client.getHttpClient();
