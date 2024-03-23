9c8019b7aa4f016d9d006b3e7cb3c2106834fba0
Om Duggineni <omduggineni@gmail.com>
2024-03-23 15:01:34 -0400

    fixed DatabaseChangeStream implementation

--- server/app/util.py
+++ server/app/util.py
@@ -1,7 +1,7 @@
 from typing import AsyncIterator
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
-
+from asyncio import Queue
 
 def handle_object_id(document):
   document["_id"] = str(document["_id"])
@@ -12,3 +12,20 @@ def stream_jsonl_response(stream: AsyncIterator[BaseModel]) -> StreamingResponse
     async for obj in stream_to_map:
       yield obj.model_dump_json() + '\n'
   return StreamingResponse(mapped_stream(stream), media_type="application/x-ndjson")
+
+class DatabaseChangeStream:
+    def __init__(self):
+        self._queues = set()
+
+    async def add_change(self, change):
+        for queue in self._queues:
+            await queue.put(change)
+    
+    async def __aiter__(self):
+        queue = Queue()
+        self._queues.add(queue)
+        try:
+            while True:
+                yield await queue.get()
+        except StopAsyncIteration:
+            self._queues.remove(queue)
