002d38a131bbd6b28bae0d08c43a9f655f50ef08
Saloni <salonigupta12405@gmail.com>
2024-03-26 16:54:51 -0400

    main script modified

--- server/scripts/add_teams.py
+++ server/scripts/add_teams.py
@@ -4,8 +4,6 @@ import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
 import sys
 sys.path.append('../')
-from app import database
-from bson import ObjectId
 
 class Add_Teams:
     headers = {
@@ -21,15 +19,6 @@ class Add_Teams:
         "players":[],
         "invitations": []
     }
-    class hunter:
-        def __init__(self):
-            self.collection = database.get_collection("hunts")
-
-        def find_hunt_id(self):
-            hunt_id = self.collection.find({
-            "_id": { }
-            })
-            return hunt_id
 
     def change(self,hunt_id, name, teamLead, players, invitation):
         self.json_data['hunt_id'] = hunt_id
@@ -41,9 +30,9 @@ class Add_Teams:
     def add(self, hunt_id, team_id):
         #Players = [{"65e8d7479bf978a5b7c2dfbb", "timeJoined": datetime.now().strftime("%Y-%m-%d %I:%M %p")},]
         self.change(hunt_id, "BEAMTEAM", "65e8d7479bf978a5b7c2dfbb", [], [])
-        response = requests.post('http://localhost:8001/teams/create_team', headers=self.headers, json=self.json_data)
+        requests.post('http://localhost:8001/teams/create_team', headers=self.headers, json=self.json_data)
 
         #Players = [{"65e8d8d29bf978a5b7c2dfbc", "timeJoined": datetime.now().strftime("%Y-%m-%d %I:%M %p")},]
         self.change(hunt_id, "DREAMTEAM", team_id, [], [])
-        response = requests.post('http://localhost:8001/teams/create_team', headers=self.headers, json=self.json_data)
+        requests.post('http://localhost:8001/teams/create_team', headers=self.headers, json=self.json_data)
 
